30901529f3c5102e0cafd5ad919d5a56b72a7573
katafrakt <inquebrantable@gmail.com>
2015-05-13 17:48:27 +0200

    unidecoder / init commit

---
+++ source/app.d
@@ -0,0 +1,15 @@
+import std.stdio;
+import stringex.unidecode;
+
+void main()
+{
+	auto string1 = "ąa ę śvć Ā";
+	auto string2 = "さらにブリーダーズカップ・クラシックを勝つなどG1を5勝する活躍を見せ";
+	
+	auto decoder = new UniDecoder();
+	writeln(string2);
+	writeln(decoder.decode(string2));
+
+	writeln(string1);
+	writeln(string1.uni_decode());
+}
