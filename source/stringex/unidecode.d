4fc6e05c30bf24db02211b551c3345e6612f65e1
Sönke Ludwig <sludwig@rejectedsoftware.com>
2017-07-23 12:35:48 +0200

    Use a static table instead of loading YAML at runtime.

--- source/stringex/unidecode.d
+++ source/stringex/unidecode.d
@@ -1,60 +1,39 @@
 module stringex.unidecode;
-import yaml;
+
+import stringex.replacements;
 import std.utf;
 import std.format;
 import std.path;
 import std.array;
 
 class UniDecoder {
-	Node[string] yamls;
-
-	this() {
-	}
-
-	string decode(string input) {
-		string result = "";
-
-		foreach(c; input.byDchar()) {
-			auto writer = appender!string();
-			formattedWrite(writer, "x%02x", c >> 8);
-			auto code_group = writer.data;
-
-			auto grouped_point = c & 255;
-			auto decoded = getReplacement(code_group, grouped_point);
-			result ~= decoded;
-		}
-
-		return result;
-	}
-
-	void loadYml(string name) {
-		Node yaml = Loader(buildPath("unidecode-yaml", name~".yml")).load();
-		yamls[name] = yaml;
-	}
-
-	string getReplacement(string code_group, int grouped_point) {
-		auto ptr = (code_group in yamls);
-
-		if(ptr is null) {
-			loadYml(code_group);
-		}
-
-		auto replacement_array = yamls[code_group];
-		return replacement_array[grouped_point].as!string;
+	string decode(string input)
+	{
+		return unidecode(input);
 	}
 }
 
-string unidecode(string input) {
-	auto decoder = new UniDecoder();
-	return decoder.decode(input);
+string unidecode(string input)
+{
+	auto result = appender!string();
+
+	foreach(c; input.byDchar()) {
+		auto grp = c >> 8;
+		auto grouped_point = c & 255;
+
+		if (grp >= replacements.length || grouped_point >= replacements[grp].length)
+			result ~= "";
+		else
+			result ~= replacements[grp][grouped_point];
+	}
+
+	return result.data;
 }
 
 unittest {
-	auto decoder = new UniDecoder();
-
-	assert(decoder.decode("abcd") == "abcd");
-	assert(decoder.decode("ABcd") == "ABcd");
-	assert(decoder.decode("ABcd ff") == "ABcd ff");
-	assert(decoder.decode("żółw") == "zolw");
-	assert(decoder.decode("反清復明") == "Fan Qing Fu Ming ");
+	assert(unidecode("abcd") == "abcd");
+	assert(unidecode("ABcd") == "ABcd");
+	assert(unidecode("ABcd ff") == "ABcd ff");
+	assert(unidecode("żółw") == "zolw");
+	assert(unidecode("反清復明") == "Fan Qing Fu Ming ");
 }
