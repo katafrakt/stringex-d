cb9849e6a2997d868b9ec5f026b6fc44ae557e1f
Paweł Świątkowski <pawel.swiatkowski@juul.com>
2019-11-06 11:08:38 +0100

    Don't decode for character up to ASCII 128
    Apparently there are some bugs in YAMLs in part that is not used by Ruby
    Stringex. It uses a workaround for those chars so we need to use
    similar.
    
    Also extracted some part of the logic to a separate private method.

--- source/stringex/unidecode.d
+++ source/stringex/unidecode.d
@@ -5,22 +5,29 @@ import std.utf;
 import std.format;
 import std.path;
 import std.array;
+import std.conv;
 
 string unidecode(string input)
 {
 	auto result = appender!string();
 
 	foreach(c; input.byDchar()) {
-		auto grp = c >> 8;
-		auto grouped_point = c & 255;
-
-		if (grp >= replacements.length || grouped_point >= replacements[grp].length)
-			result ~= "";
-		else
-			result ~= replacements[grp][grouped_point];
+		result ~= decoded(c);
 	}
 
 	return result.data;
+}
+
+private string decoded(dchar c) {
+	if (c <= 128) return c.to!string(); // match https://github.com/rsl/stringex/blob/v2.8.5/lib/stringex/unidecoder.rb#L49
+	
+	auto grp = c >> 8;
+	auto grouped_point = c & 255;
+
+	if (grp >= replacements.length || grouped_point >= replacements[grp].length)
+		return "";
+	else
+		return replacements[grp][grouped_point];
 }
 
 unittest {
@@ -30,4 +37,10 @@ unittest {
 	assert(unidecode("żółw") == "zolw");
 	assert(unidecode("反清復明") == "Fan Qing Fu Ming ");
 	assert(unidecode("أنا قادر على أكل الزجاج و هذا لا يؤلمن") == "'n qdr 'l~ 'kl lzjj w hdh l yw'lmn");
+}
+
+unittest {
+	// regressions found in https://github.com/katafrakt/stringex-d/issues/3
+	assert(unidecode("`") == "`");
+	assert(unidecode("[") == "[");
 }
